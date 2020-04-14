398edf0a17f554cda04aedab7ce0ab7131ee2089
Paul de Laage <63097279+pdldm@users.noreply.github.com>
2020-04-15 00:28:55 +0200

    modify for watchOS support

--- Sources/Disk+VolumeInformation.swift
+++ Sources/Disk+VolumeInformation.swift
@@ -26,7 +26,7 @@ import Foundation
 /// Confirm that you have enough local storage space for a large amount of data.
 ///
 /// Source: https://developer.apple.com/documentation/foundation/nsurlresourcekey/checking_volume_storage_capacity?changes=latest_major&language=objc
-@available(iOS 11.0, *)
+
 public extension Disk {
     /// Helper method to query against a resource value key
     private static func getVolumeResourceValues(for key: URLResourceKey) -> URLResourceValues? {
@@ -55,6 +55,8 @@ public extension Disk {
     ///
     /// Indicates the amount of space that can be made available  for things the user has explicitly requested in the app's UI (i.e. downloading a video or new level for a game.)
     /// If you need more space than what's available - let user know the request cannot be fulfilled.
+    #if os(iOS)
+    @available(iOS 11.0, *)
     static var availableCapacityForImportantUsage: Int? {
         get {
             let resourceValues = getVolumeResourceValues(for: .volumeAvailableCapacityForImportantUsageKey)
@@ -70,6 +72,7 @@ public extension Disk {
     ///
     /// Indicates the amount of space available for things that the user is likely to want but hasn't explicitly requested (i.e. next episode in video series they're watching, or recently updated documents in a server that they might be likely to open.)
     /// For these types of files you might store them initially in the caches directory until they are actually used, at which point you can move them in app support or documents directory.
+    @available(iOS 11.0, *)
     static var availableCapacityForOpportunisticUsage: Int? {
         get {
             let resourceValues = getVolumeResourceValues(for: .volumeAvailableCapacityForOpportunisticUsageKey)
@@ -80,4 +83,5 @@ public extension Disk {
             }
         }
     }
+    #endif
 }
